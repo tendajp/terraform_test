0a957c1fa2dc39662c975f7439da3c1789714c06
tendajp <118061803+tendajp@users.noreply.github.com>
2023-06-10 17:39:08 +0200

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,11 @@
 variable "resource_group" {
   description = "The resource group"
-  default = "terraform-test-rg"
+  default = "terraform-test1-rg"
 }
 
 variable "application_name" {
   description = "The Spring Boot application name"
-  default     = "cwiczenia"
+  default     = "cwiczenia1"
 }
 
 variable "location" {
